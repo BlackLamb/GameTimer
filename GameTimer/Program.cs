446dfd0e1977c56f905d588fdcc2404b90fff826
Brent Lamb <blamb@fathomgame.com>
2012-08-12 01:01:36 -0700

    Added settings mode
    Added a settings mode with a 2, 5, 8, 10 min setting. Settings mode uses
    blue leds and on poweron device is set to 5 min.

--- GameTimer/Program.cs
+++ GameTimer/Program.cs
@@ -15,6 +15,8 @@ namespace GameTimer
         static bool blinkOff = false;
         static int timerMin = 5;
         static int scale;
+        static timeModes mode = timeModes.five;
+        static bool changeMode = false;
 
 
         // Gobus modules
@@ -33,30 +35,100 @@ namespace GameTimer
             led3 = new NetduinoGo.RgbLed(GoSockets.Socket6);
             button1 = new NetduinoGo.Button(GoSockets.Socket1);
             button2 = new NetduinoGo.Button(GoSockets.Socket3);
+            InterruptPort settingButton = new InterruptPort(Pins.Button, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeLow);
+            settingButton.OnInterrupt += new NativeEventHandler(settingButton_OnInterrupt);
+            OutputPort powerlight = new OutputPort(Pins.PowerLed, false);
+
 #if !mute
             buzzer = new NetduinoGo.PiezoBuzzer(); 
 #endif
 
             // Set Scale
-            scale = (timerMin * 60) / 5;
+            SetScale();
 
             // Register Buttons
             button1.ButtonPressed += new NetduinoGo.Button.ButtonEventHandler(button1_ButtonPressed);
             button2.ButtonPressed += new NetduinoGo.Button.ButtonEventHandler(button2_ButtonPressed);
-
+            led2.SetColor(0, 0, 255);
             // Main thread sleep time
             Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void SetScale()
+        {
+            scale = (timerMin * 60) / 5;
+        }
+
+        static void settingButton_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            ResetTimer();
+            switch (mode)
+            {
+                case timeModes.two:
+                    led1.SetColor(0, 0, 255);
+                    break;
+                case timeModes.five:
+                    led2.SetColor(0, 0, 255);
+                    break;
+                case timeModes.eight:
+                    led3.SetColor(0, 0, 255);
+                    break;
+                case timeModes.ten:
+                    led1.SetColor(0, 0, 255);
+                    led2.SetColor(0, 0, 255);
+                    led3.SetColor(0, 0, 255);
+                    break;
+            }
+            changeMode = true;
         }
 
         // Reset Button
         static void button1_ButtonPressed(object sender, bool buttonState)
         {
-            ResetTimer();
+            if (changeMode)
+            {
+                mode++;
+                if ((int)mode > 3)
+                    mode = 0;
+                switch (mode)
+                {
+                    case timeModes.two:
+                        timerMin = 2;
+                        led1.SetColor(0, 0, 255);
+                        led2.SetColor(0, 0, 0);
+                        led3.SetColor(0, 0, 0);
+                        break;
+                    case timeModes.five:
+                        timerMin = 5;
+                        led1.SetColor(0, 0, 0);
+                        led2.SetColor(0, 0, 255);
+                        led3.SetColor(0, 0, 0);
+                        break;
+                    case timeModes.eight:
+                        timerMin = 8;
+                        led1.SetColor(0, 0, 0);
+                        led2.SetColor(0, 0, 0);
+                        led3.SetColor(0, 0, 255);
+                        break;
+                    case timeModes.ten:
+                        timerMin = 10;
+                        led1.SetColor(0, 0, 255);
+                        led2.SetColor(0, 0, 255);
+                        led3.SetColor(0, 0, 255);
+                        break;
+                }
+                SetScale();
+            }
+            else
+            {
+                ResetTimer();
+            }
         }
 
         // Start/Stop button
         static void button2_ButtonPressed(object sender, bool buttonState)
         {
+            changeMode = false;
             if (gameTimer != null)
             {
                 ResetTimer();
@@ -171,5 +243,13 @@ namespace GameTimer
             led3.SetColor(0, 0, 0);
             timeCount = 0;
         }
+    }
+
+    enum timeModes
+    {
+        two,
+        five,
+        eight,
+        ten
     }
 }
